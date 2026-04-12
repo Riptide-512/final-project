4aa00ceb9ba0158641601366839b90e907284d44
Riptide-512 <eric.ivan.sepulveda@gmail.com>
2026-04-12 16:41:40 -0500

    adding wrangler and cloudflare

--- template/vite.config.js
+++ template/vite.config.js
@@ -1,16 +1,9 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 import tailwindcss from '@tailwindcss/vite'
-
+import { cloudflare } from "@cloudflare/vite-plugin";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
+  plugins: [vue(), tailwindcss(), cloudflare()],
 })
