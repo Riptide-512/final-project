dd5df3433395354906e29787a0d23cbd78e265a6
Riptide-512 <eric.ivan.sepulveda@gmail.com>
2026-05-11 23:58:10 -0500

    Adding .env and installing globals

--- template/eslint.config.js
+++ template/eslint.config.js
@@ -1,19 +1,25 @@
 import js from '@eslint/js'
+import globals from 'globals'
 import pluginVue from 'eslint-plugin-vue'
 import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default [
   {
     name: 'app/files-to-lint',
-    files: ['**/*.{js,mjs,jsx,vue']
+    files: ['**/*.{js,mjs,jsx,vue}'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
   },
+
   {
     name: 'app/files-to-ignore',
     ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**', '**/node_modules/**'],
-
   },
+
   js.configs.recommended,
   ...pluginVue.configs['flat/recommended'],
-  eslintConfigPrettier
-
+  eslintConfigPrettier,
 ]
